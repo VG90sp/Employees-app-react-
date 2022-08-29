edcdf6496a98bcefce6a2e45ef048a75c44d67e8
Vladimir Grigoriev <Vladimir.Grigoriev@volnamobile.ru>
2022-08-29 10:40:43 +0300

    add all components

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,10 +1,20 @@
+import AppFilter from "../app-filter/app-filter";
 import { AppInfo } from "../app-info/app-info";
+import EmployeesList from "../employees-list/employees-list";
+import SearchPanel from "../search-panel/search-panel";
+import EmployeesAddForm from "../employees-add-form/employees-add-form";
 import "./app.css";
 
 function App() {
   return (
     <div className="app">
       <AppInfo />
+      <div className="search-panel">
+        <SearchPanel />
+        <AppFilter />
+      </div>
+      <EmployeesList />
+      <EmployeesAddForm />
     </div>
   );
 }
